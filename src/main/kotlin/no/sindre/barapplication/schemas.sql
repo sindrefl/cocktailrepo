007ab78d6c2436a6ccab608303d95b6738bf4f5a
Sindre Flood <sindre.flood@bekk.no>
2018-09-30 09:11:12 +0200

    added scripts to populate db

--- src/main/kotlin/no/sindre/barapplication/schemas.sql
+++ src/main/kotlin/no/sindre/barapplication/schemas.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS cocktail_db.INGREDIENT;
 DROP TABLE IF EXISTS cocktail_db.CATEGORY;
 
 CREATE TABLE cocktail_db.COCKTAIL(
-id SERIAL NOT NULL,
+cocktail_id SERIAL NOT NULL,
 name VARCHAR(50),
 description VARCHAR(100),
 glass VARCHAR(20),
@@ -12,29 +12,29 @@ category VARCHAR(50),
 alcoholic VARCHAR(20),
 img_link VARCHAR(100),
 recipe VARCHAR(1000),
-PRIMARY KEY(id)
+PRIMARY KEY(cocktail_id)
 );
 
 CREATE TABLE cocktail_db.INGREDIENT(
-id SERIAL NOT NULL,
+ingredient_id SERIAL NOT NULL,
 name VARCHAR(255),
 ing_description VARCHAR(255),
 type VARCHAR(50),
-isBattery BOOLEAN,
-PRIMARY KEY(id)
+isBattery INTEGER,
+PRIMARY KEY(ingredient_id)
 );
 
 CREATE TABLE cocktail_db.COCKTAILHASINGREDIENT(
 cocktail_id INTEGER NOT NULL,
 ingredient_id INTEGER NOT NULL,
 amount VARCHAR(50),
-FOREIGN KEY(cocktail_id) REFERENCES COCKTAIL(id),
-FOREIGN KEY(ingredient_id) REFERENCES INGREDIENT(id)
+FOREIGN KEY(cocktail_id) REFERENCES cocktail_db.COCKTAIL(cocktail_id),
+FOREIGN KEY(ingredient_id) REFERENCES cocktail_db.INGREDIENT(ingredient_id)
 );
 
 CREATE TABLE cocktail_db.CATEGORY(
-id SERIAL NOT NULL,
+category_id SERIAL NOT NULL,
 name VARCHAR(50),
 cat_description VARCHAR(255),
-PRIMARY KEY(id)
+PRIMARY KEY(category_id)
 );
